e62516a5432b5a2ab524c7aa1ff156ad763c2763
Will Andre <wandre94@gmail.com>
2021-02-04 18:45:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,7 +4,7 @@ class Song
   extend Memorable
   extend Findable
   include Paramable
-  
+
   attr_accessor :name
   attr_reader :artist
 
@@ -22,7 +22,4 @@ class Song
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
 end
