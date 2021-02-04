5692e87b5ba77cc40330e02913dbc805a1d2bc8f
Will Andre <wandre94@gmail.com>
2021-02-04 18:33:28 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,7 @@
 require 'pry'
 
 class Artist
+  extend Memorable
   attr_accessor :name
   attr_reader :songs
 
@@ -17,14 +18,6 @@ class Artist
 
   def self.all
     @@artists
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def add_song(song)
